d24841cbd45a8d484ca9dde223767ae0378f59d5
Phebe Sawan <phebe.sawan@devpartners.co>
2020-06-20 06:26:11 +0800

    LATE UPDATES: use moxios to prevent actual api calls for App routing

--- src/containers/App.router.test.js
+++ src/containers/App.router.test.js
@@ -3,8 +3,9 @@ import userEvent from '@testing-library/user-event';
 import React from 'react';
 import configureStore from 'redux-mock-store'
 import { Provider } from 'react-redux';
-import App from './containers/App';
+import App from './App';
 import thunk from 'redux-thunk';
+import moxios from 'moxios';
 
 const mockUser = {
   id: 2123,
@@ -37,10 +38,16 @@ const setup = () => {
   );
 };
 
-test('navigates to user posts when clicking a user', () => {
+beforeEach(() => {
+  moxios.install();
+});
+afterEach(() => {
+  moxios.uninstall();
+})
+
+test('clicking a user navigates to user posts', () => {
   setup();
   const element = screen.getByTestId('user-item');
   userEvent.click(element);
   expect(window.location.href).toEqual(`http://localhost/users/${mockUser.id}`);
 });
-
